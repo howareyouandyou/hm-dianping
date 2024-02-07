2c13738035f2e0b641c7ff3ae8f90d28e6d543bd
404 <3260969751@qq.com>
2024-02-07 11:06:31 +0800

    3.优惠券秒杀

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +27,87 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        //查询优惠券
+        SeckillVoucher voucher=seckillVoucherService.getById(voucherId);
+        //判断秒杀是否开始
+        if(voucher.getBeginTime().isAfter(LocalDateTime.now()))
+        {
+            return Result.fail("秒杀尚未开始！");
+        }
+        //判断秒杀是否已经结束
+        if(voucher.getEndTime().isBefore(LocalDateTime.now()))
+        {
+            return Result.fail("秒杀已经结束！");
+        }
+        //判断库存是否充足
+        if(voucher.getStock()<1)
+        {
+            //库存不足
+            return Result.fail("库存不足！");
+        }
+
+
+        Long userId = UserHolder.getUser().getId();
+        /*
+           只有同一个用户（userId相同）时才需要加锁
+         */
+        synchronized (userId.toString().intern())
+        {
+            //获取代理对象（事务）
+            IVoucherOrderService proxy=(IVoucherOrderService) AopContext.currentProxy();
+            return proxy.creatVocherOrder(voucherId);
+        }
+
+
+    }
+
+    @Transactional
+    public Result creatVocherOrder(Long voucherId) {
+        //一人一单
+        Long userId = UserHolder.getUser().getId();
+
+        //查询订单
+        int count = query().eq("user_id",userId).eq("voucher_id", voucherId).count();
+        //判断是否存在
+        if(count>0)
+        {
+            //用户已经购买过了
+            return Result.fail("用户已经购买过一次！");
+        }
+
+        //扣减库存（两张表的都要扣减
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock-1") //set stock = stock-1
+                .eq("voucher_id",voucherId).gt("stock",0)//where id=? and stock=?
+                .update();
+        if(!success)
+        {
+            //扣减失败
+            return Result.fail("库存不足！");
+        }
+
+        //创建订单
+        VoucherOrder voucherOrder=new VoucherOrder();
+        //获取订单id
+        long orderId=redisIdWorker.nextId("order");
+        voucherOrder.setId(orderId);
+        //用户id
+        voucherOrder.setUserId(userId);
+        //代金券id
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+
+        //返回订单id
+        return Result.ok(orderId);
+
+
+    }
 }
---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,59 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author 86152
+ * @version 1.0
+ * Create by 2024/2/6 16:37
+ */
+@Component
+public class RedisIdWorker {
+
+    /**
+     * 开始时间戳
+     */
+    private static final long BEGIN_TIMESTAMP = 1640995200L;
+    /**
+     * 序列号的位数
+     */
+    private static final int COUNT_BITS = 32;
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    /**
+     * 生成全局唯一ID
+     * @param keyPrefix 业务前缀
+     * @return
+     */
+    public long nextId(String keyPrefix)
+    {
+        // 1.生成时间戳
+        LocalDateTime now=LocalDateTime.now();
+        long nowSecond=now.toEpochSecond(ZoneOffset.UTC);
+        long timetamp=nowSecond-BEGIN_TIMESTAMP;
+
+        // 2.生成序列号
+        // 2.1.获取当前日期，精确到天
+        String date=now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+
+        // 2.2.自增长
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+
+        // 3.拼接并返回
+        return timetamp<<COUNT_BITS | count;
+
+    }
+
+
+
+}
